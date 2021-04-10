9d7523554aa8a8906079b7ad9bac247ce3c2b73f
danmun <daniel.munkacsi@gmail.com>
2021-04-11 00:40:02 +0900

    fix nullpointer

--- middleware.js
+++ middleware.js
@@ -17,14 +17,13 @@ const logout = (req, res) => {
 const login = (req, res) => {
     const { username, password } = req.body;
     User.findOne({ username }, function(err, user) {
-        const _id = user._id;
         if (err) {
-            console.error(err);
             res.status(500).json({error: STRINGS.SERVER_ERROR});
         } else if (!user) {
             res.status(401).json({error: STRINGS.LOGIN_FAILURE});
         } else {
             user.isCorrectPassword(password, function(err, same) {
+                const _id = user._id;
                 if (err) {
                     res.status(500).json({error: STRINGS.SERVER_ERROR});
                 } else if (!same) {
